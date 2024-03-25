c2994a71921dc9e57c4f937de173b56670749d85
Ankit Rawat <156670935+anki0010@users.noreply.github.com>
2024-03-25 20:53:35 +0200

    part 3 todo done

--- js/index.js
+++ js/index.js
@@ -1,16 +1,62 @@
+
+const BACKEND_ROOT_URL = 'http://localhost:3001';
+
 const list = document.querySelector('ul');
 const input = document.querySelector('input');
 
-input.addEventListener('keypress', (event) => { 
-    if (event.key === 'Enter') { 
-        event.preventDefault();
+input.disabled = true;
+
+
+const renderTask = (task) => {
+    const li = document.createElement('li');
+    li.setAttribute('class', 'list-group-item');
+    li.innerHTML = task;
+    list.appendChild(li);
+}
+
+const getTasks = async () => {
+    try {
+        const response = await fetch(BACKEND_ROOT_URL);
+        const json = await response.json();
+        json.forEach(task => {
+            renderTask(task.description);
+        });
+        input.disabled = false;
+    } catch (error) {
+        console.error('Error:', error);
+        alert('Error fetching tasks' + error);
+    }
+}
+
+const saveTask = async (task) => {
+    try {
+        const json = JSON.stringify({description: task});
+        const response = await fetch(BACKEND_ROOT_URL+ "/new", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: json
+        });
+        return response.json();
+    } catch (error) {
+        console.error('Error:#', error);
+        alert('Error saving task#' + error.message);
+    }
+}
+
+getTasks();
+
+
+input.addEventListener('keypress', (e) => {
+    if(e.key === 'Enter') {
+        e.preventDefault();
         const task = input.value.trim();
-        if (task !== '') { 
-            const li = document.createElement('li');
-            li.setAttribute('class', 'list-group-item');
-            li.innerHTML = task;
-            list.append(li);
-            input.value = ''; 
-        }
+        if (task !=='') {
+            saveTask(task).then((json) => {
+                renderTask(task);
+                input.value = '';
+            });
+        } 
     }
 });
--- server/index.js
+++ server/index.js
@@ -1,46 +1,53 @@
+
 const express = require('express');
-const cors = require ('cors');
-const { Pool } = require('pg')
-const app = express()
-app.use(cors())
-app.use(express.json())
-const port = 3001
+const cors = require('cors');
+const { Pool } = require('pg');
 
-app.get ("/", (req, res) => {
-    const pool = openDb()
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-    pool.query('SELECT * FROM task', (error, result) => {
-        if ( error ){
-            res.status(500).json({error: error.message })
-        }
-        
-    })
-    res.status(200).json({result: 'success'})
-    
-})
+const port = 3001;
 
-app.listen(port);
 
 const openDb = () => {
     const pool = new Pool({
         user: 'postgres',
         host: 'localhost',
         database: 'todo',
-        password: '123',
-        port: 5432
-    })
-    return pool
-};
+        password: 'salasana',
+        port: 5432,
 
-app.post ("/new",(req,res) => {
-    const pool = openDb()
-    pool.query('insert into task (description) values ($1) returning *',
-    [req.body.description],
-    (error,result) => {
+    });
+    return pool;
+}
+
+app.get('/', (req, res) => {
+    const pool = openDb();
+
+    pool.query('SELECT * FROM task', (error, result) => {
         if (error) {
+            res.status(500).send({error: error.message});
+        }
+        res.status(200).send(result.rows);
+    });
+});
+
+app.post('/new', (req, res) => {
+    const pool = openDb();
+
+    pool.query("INSERT INTO task (description) VALUES ($1) returning *",
+     [req.body.description], (error, result) => {
+        if (error) {
+            console.error('Error executing query:', error);
             res.status(500).json({error: error.message});
         } else {
-            res.status(200).json({id : result.rows[0].id})
+            console.log('Query result:', result);
+            res.status(201).json({id : result.rows[0].id});
         }
-    })
+    });
 });
+
+
+
+app.listen(port);
